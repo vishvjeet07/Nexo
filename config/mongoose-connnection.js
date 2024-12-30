7150253e920180939696a17fadfaf355cbf171e9
vishvjeet07 <vishvjeetwankhade07@gmail.com>
2024-12-30 13:01:04 +0530

    database connected

--- config/mongoose-connnection.js
+++ config/mongoose-connnection.js
@@ -3,8 +3,9 @@ const config = require('config');
 
 const dbgr = require('debug')("development:mongoose");
 
-mongoose.connect(`${config.get("MONGODB_URI")}/sneakers`)
+mongoose.connect(process.env.MONGODB_URI)
 .then(function(){
+    console.log("connected");
 })
 .catch(function(err){
     dbgr(err);
